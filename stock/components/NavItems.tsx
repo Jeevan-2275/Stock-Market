9f9771acaeb363196edfbf1843eea49c49e4720d
Jeevan-2275 <jeevankadam2275@gmail.com>
2025-10-12 13:10:19 +0530

    ✨ add Header.tsx and NavItems.tsx components

---
+++ stock/components/NavItems.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavItems = () => {
+  return (
+    <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
+        {[
+            { title: 'Home', link: '/' },
+        ]}
+    </ul>
+  )
+}
+
+export default NavItems

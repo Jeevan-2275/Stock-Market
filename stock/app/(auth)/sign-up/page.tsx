e74123e3cbfec15b123f8c103a2929421b257f20
Jeevan-2275 <jeevankadam2275@gmail.com>
2025-11-13 19:22:26 +0530

    refactor: enhance sign-up page and integrate CountrySelectField fixes

--- stock/app/(auth)/sign-up/page.tsx
+++ stock/app/(auth)/sign-up/page.tsx
@@ -6,7 +6,7 @@ import InputField from "@/components/forms/InputField";
 import SelectField from "@/components/forms/SelectField";
 import {INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS} from "@/lib/constants";
 import {CountrySelectField} from "@/components/forms/CountrySelectField";
-// import FooterLink from "@/components/forms/FooterLink";
+import FooterLink from "@/components/forms/FooterLink";
 // import {signUpWithEmail} from "@/lib/actions/auth.actions";
 import {useRouter} from "next/navigation";
 // import {toast} from "sonner";
@@ -33,13 +33,14 @@ const SignUp = () => {
 
     const onSubmit = async (data: SignUpFormData) => {
         try {
-            const result = await signUpWithEmail(data);
-            if(result.success) router.push('/');
+            console.log(data);
+            // const result = await signUpWithEmail(data);
+            // if(result.success) router.push('/');
         } catch (e) {
             console.error(e);
-            toast.error('Sign up failed', {
-                description: e instanceof Error ? e.message : 'Failed to create an account.'
-            })
+            // toast.error('Sign up failed', {
+            //     description: e instanceof Error ? e.message : 'Failed to create an account.'
+            // })
         }
     }
 
@@ -118,7 +119,7 @@ const SignUp = () => {
                     {isSubmitting ? 'Creating Account' : 'Start Your Investing Journey'}
                 </Button>
 
-    {/* //             <FooterLink text="Already have an account?" linkText="Sign in" href="/sign-in" /> */}
+           <FooterLink text="Already have an account?" linkText="Sign in" href="/sign-in" />
             </form>
         </>
     )
